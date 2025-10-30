9ed7e289830bdfa8a986f87a72baa947ad8a247f
dileep0306 <dileeplesanr315@gmai.com>
2025-10-30 23:59:53 +0530

    Updated CORS for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,12 +20,23 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+
+// for local development only
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+// dynamic cors configuration for production and development
+app.use(cors({
+  origin: process.env.NODE_ENV === "production" 
+    ? process.env.FRONTEND_URL  // Should be: https://pulsechat-frontend.onrender.com
+    : "http://localhost:5173",
+  credentials: true,
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
